cfd7be3c05a5b2072f0d6f2fa41bb71afef4e1b6
Petr U <petrakliy@gmail.com>
2023-06-06 15:49:48 +0300

    added min websites interface

--- internal/app/website/website.go
+++ internal/app/website/website.go
@@ -1,9 +1,35 @@
 package website
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Website struct {
-	URL       string
-	Status    bool
-	LastCheck time.Time
+	URL        string
+	ShortName  string
+	Status     bool
+	LastCheck  time.Time
+	AccessTime int64
+	MinTime    int64
+	MaxTime    int64
+}
+
+// 1. Получить время доступа к определенному сайту.
+// 2. Получить имя сайта с минимальным временем доступа.
+// 3. Получить имя сайта с максимальным временем доступа.
+type WebsiteStorage interface {
+	GetAccessTime(ctx context.Context, shortURL string) (int64, error)
+	GetMinAccessURL(ctx context.Context) (string, error)
+	GetMaxAccessURL(ctx context.Context) (string, error)
+}
+
+type Websites struct {
+	webstore WebsiteStorage
+}
+
+func NewUsers(webstore WebsiteStorage) *Websites {
+	return &Websites{
+		webstore: webstore,
+	}
 }
