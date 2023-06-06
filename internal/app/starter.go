a20d3df887e14ada9429297bbbd61d93265064f0
Petr U <petrakliy@gmail.com>
2023-06-06 16:48:03 +0300

    added app layer

---
+++ internal/app/starter.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"context"
+	"sync"
+
+	"github.com/larikhide/website-monitor/internal/app/website"
+)
+
+type App struct {
+	ws *website.Websites
+}
+
+func NewApp(ws website.WebsiteStorage) *App {
+	a := &App{
+		ws: website.NewWebsites(ws),
+	}
+	return a
+}
+
+type APIServer interface {
+	Start(ws *website.Websites)
+	Stop()
+}
+
+func (a *App) Serve(ctx context.Context, wg *sync.WaitGroup, hs APIServer) {
+	defer wg.Done()
+	hs.Start(a.ws)
+	<-ctx.Done()
+	hs.Stop()
+}
