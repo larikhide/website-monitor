186f80b520a349760d9c7755658b4f160650f83e
Petr U <petrakliy@gmail.com>
2023-06-06 18:15:01 +0300

    fixed handler

--- internal/api/handler/handler.go
+++ internal/api/handler/handler.go
@@ -1,22 +1,28 @@
 package handler
 
 import (
-	"context"
+	"net/http"
 
 	"github.com/larikhide/website-monitor/internal/app/website"
 )
 
-type Handler struct {
+type Router struct {
+	*http.ServeMux
 	ws *website.Websites
 }
 
-func NewHandler(ws *website.Websites) *Handler {
-	h := &Handler{
-		ws: ws,
+func NewRouter(ws *website.Websites) *Router {
+	r := &Router{
+		ServeMux: http.NewServeMux(),
+		ws:       ws,
 	}
-	return h
+	r.Handle("/getAccessTime", r.AuthMiddleware(http.HandlerFunc(r.GetAccessTime)))
+	r.Handle("/GetMinAccessURL", r.AuthMiddleware(http.HandlerFunc(r.GetMinAccessURL)))
+	r.Handle("/GetMinAccessURL", r.AuthMiddleware(http.HandlerFunc(r.GetMaxAccessURL)))
+	return r
 }
 
-func (hs *Handler) GetAccessTime(ctx context.Context, shortURL string) (int64, error) {
-
-}
+func (rt *Router) AuthMiddleware(next http.Handler) http.Handler          {}
+func (rt *Router) GetAccessTime(w http.ResponseWriter, r *http.Request)   {}
+func (rt *Router) GetMinAccessURL(w http.ResponseWriter, r *http.Request) {}
+func (rt *Router) GetMaxAccessURL(w http.ResponseWriter, r *http.Request) {}
