ead142435ef3a586283cf6e7f1fac0965b814a87
Petr U <petrakliy@gmail.com>
2023-06-09 13:38:41 +0300

    added stats repo interface

--- internal/app/repos/stats/stats.go
+++ internal/app/repos/stats/stats.go
@@ -2,43 +2,30 @@ package stats
 
 import (
 	"context"
-	"fmt"
 )
 
 type Stats struct {
-	SlowestCounter int64
-	FastestCounter int64
+	MinPingURL          string
+	MaxPingURL          string
+	MinPingRequestCount int
+	MaxPingRequestCount int
 }
 
-// for admins
-// 2. Получить статистику запросов к минимальному времени доступа.
-// 3. Получить статистику запросов к максимальному времени доступа.
-type StatsStorage interface {
-	GetMinAccessURLStats(ctx context.Context) (int64, error)
-	GetMaxAccessURLStats(ctx context.Context) (int64, error)
+// 2. Получить имя сайта с минимальным временем доступа.
+// 3. Получить имя сайта с максимальным временем доступа.
+// 5. Получить статистику запросов к минимальному времени доступа.
+// 6. Получить статистику запросов к максимальному времени доступа.
+type StatsRepository interface {
+	Read(ctx context.Context) (*Stats, error)
+	Update(ctx context.Context, upds *Stats) error
 }
 
 type Statistics struct {
-	statstore StatsStorage
+	statstore StatsRepository
 }
 
-func NewStatistics(sstore StatsStorage) *Statistics {
+func NewStatistics(sstore StatsRepository) *Statistics {
 	return &Statistics{
 		statstore: sstore,
 	}
-}
-
-func (ss *Statistics) ReadMinAccessURLStats(ctx context.Context) (int64, error) {
-	minAccessStats, err := ss.statstore.GetMinAccessURLStats(ctx)
-	if err != nil {
-		return 0, fmt.Errorf("get from db errors: %w", err)
-	}
-	return minAccessStats, nil
-}
-func (ss *Statistics) GetMaxAccessURLStats(ctx context.Context) (int64, error) {
-	maxAccessStats, err := ss.statstore.GetMaxAccessURLStats(ctx)
-	if err != nil {
-		return 0, fmt.Errorf("get from db errors: %w", err)
-	}
-	return maxAccessStats, nil
 }
--- internal/app/repos/website/website.go
+++ internal/app/repos/website/website.go
@@ -13,24 +13,25 @@ type Website struct {
 	PingRequestsCounter int64
 }
 
-// for users
 // 1. Получить время доступа к определенному сайту.
-// 2. Получить имя сайта с минимальным временем доступа.
-// 3. Получить имя сайта с максимальным временем доступа.
-type WebsiteStorage interface {
+type WebsiteRepository interface {
 	//Create(ctx context.Context, website Website) (string, error)
 	Read(ctx context.Context, url string) (*Website, error)
 	Update(ctx context.Context, website *Website) error
 	//Delete(ctx context.Context, url string) error
 
+	//
 	GetWebsitesList(ctx context.Context) ([]Website, error)
+
+	GetPingRequestCount(ctx context.Context, url string) (int, error)
+	IncrementPingRequestCount(ctx context.Context, url string) error
 }
 
 type Websites struct {
-	wstore WebsiteStorage
+	wstore WebsiteRepository
 }
 
-func NewWebsites(wstore WebsiteStorage) *Websites {
+func NewWebsites(wstore WebsiteRepository) *Websites {
 	return &Websites{
 		wstore: wstore,
 	}
